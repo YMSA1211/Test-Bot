b42f7f3a6a4e119ce4c0eb993804552037ed488d
YMSA1211 <45988458+YMSA1211@users.noreply.github.com>
2019-01-05 12:41:59 +0300

    Create Bot.js

---
+++ Bot.js
@@ -0,0 +1,11 @@
+```js
+client.on('ready', () => {
+   console.log(`NDQ2OTk5MjIzMTgzOTk4OTg3.DwEaWA.CApNEI-EFXGBDyzWNy3M_ZOZgH4`);
+      console.log(`NDQ2OTk5MjIzMTgzOTk4OTg3.DwEaWA.CApNEI-EFXGBDyzWNy3M_ZOZgH4`);
+        console.log(`----------------`);
+      console.log(`ON ${client.guilds.size} Servers '     Script By : Venom#4403 ' `);
+    console.log(`NDQ2OTk5MjIzMTgzOTk4OTg3.DwEaWA.CApNEI-EFXGBDyzWNy3M_ZOZgH4`);
+  console.log(`Logged in as ${client.user.tag}!`);
+client.user.setGame(`اينما وجد الحب وجد السلام ❤`,"http://twitch.tv/ahmad107996")
+client.user.setStatus("dnd")
+});
